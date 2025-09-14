82ea037449b0a6038f1a7f5766b94c3de7d777b0
Seyoung Han <89440450+1seyoung@users.noreply.github.com>
2025-09-14 11:34:01 +0900

    Title: 더 긴 문자열, Time: 119ms, Memory: 10MB, Status: Passed - Codetree

--- 250914/더 긴 문자열/longer-string.java
+++ 250914/더 긴 문자열/longer-string.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
         String line = sc.nextLine();
         String[] arr = line.split(" ");
 
