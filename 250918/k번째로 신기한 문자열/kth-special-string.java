fb941b3c654aa2beac584e6e6971a1afa290c69b
Seyoung Han <89440450+1seyoung@users.noreply.github.com>
2025-09-18 05:46:33 +0900

    Title: k번째로 신기한 문자열, Time: 147ms, Memory: 11MB, Status: Runtime Error - Codetree

--- 250918/k번째로 신기한 문자열/kth-special-string.java
+++ 250918/k번째로 신기한 문자열/kth-special-string.java
@@ -9,11 +9,14 @@ public class Main {
         int m = 0;
         for (int i = 0; i < n; i++) {
             String s = sc.next();
-            String part = s.substring(0,2);
-            if(part.equals(t)){
-                words[m] = s;
-                m+=1;
+            if (s.length() >= 2){
+                String part = s.substring(0,2);
+                if(part.equals(t)){
+                    words[m] = s;
+                    m+=1;
+                }
             }
+
         }
         // Please write your code here.
 
