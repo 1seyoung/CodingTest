bdad4fd0041926398c1c746790f6ba3b59ec0f86
Seyoung Han <89440450+1seyoung@users.noreply.github.com>
2025-11-04 00:36:40 +0900

    Title: 흰검 칠하기, Time: 452ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

---
+++ 251104/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class Main {
+    static char[][] tile;
+    static char[] lastColor;
+    static int curIdx;
+    static int line;
+    static int offset = 200001; // 음수 좌표 방지용
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        tile = new char[n][offset];
+        lastColor = new char[offset];
+        curIdx = offset / 2; // 시작점을 가운데로
+        line = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            char dir = sc.next().charAt(0);
+            coloringTile(x, dir);
+        }
+
+        int wCount = 0;
+        int bCount = 0;
+        int gCount = 0;
+
+        // 각 칸의 최종 색상 통계 계산
+        for (int j = 0; j < offset; j++) {
+            int blackCnt = 0;
+            int whiteCnt = 0;
+
+            for (int i = 0; i < n; i++) {
+                if (tile[i][j] == 'B') blackCnt++;
+                if (tile[i][j] == 'W') whiteCnt++;
+            }
+
+            if (blackCnt >= 2 && whiteCnt >= 2) {
+                gCount++;
+            } else {
+                if (lastColor[j] == 'W') wCount++;
+                if (lastColor[j] == 'B') bCount++;
+            }
+        }
+
+        System.out.println(wCount + " " + bCount + " " + gCount);
+        sc.close();
+    }
+
+    public static void coloringTile(int x, char dir) {
+        if (dir == 'R') {
+            for (int i = curIdx; i < curIdx + x; i++) {
+                tile[line][i] = 'B';
+                lastColor[i] = 'B';
+            }
+            if (x > 1) curIdx += x - 1; // 현재 위치 포함이므로 x-1칸만 이동
+            line++;
+        } else { // L
+            for (int i = curIdx; i > curIdx - x; i--) {
+                tile[line][i] = 'W';
+                lastColor[i] = 'W';
+            }
+            if (x > 1) curIdx -= x - 1;
+            line++;
+        }
+    }
+}
