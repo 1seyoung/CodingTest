067802dd63fa9f9a090cfbf9442ef0fa2643a471
Seyoung Han <89440450+1seyoung@users.noreply.github.com>
2025-11-04 00:57:18 +0900

    Title: 신기한 타일 뒤집기, Time: 210ms, Memory: 14MB, Status: Wrong Answer - Codetree

---
+++ 251104/신기한 타일 뒤집기/strange-flipping-tiles.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int offset = 200001;
+        int curPos = offset / 2;
+
+        char[] block = new char[offset];
+
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            if(d == 'R'){
+                // 검은색
+                for(int j = curPos; j< curPos+x; j++){
+                    block[j] = 'B';
+                }
+
+                curPos = curPos +x-1;
+            }
+
+            if(d == 'L'){
+                // 흰색
+                for(int j = curPos; j> curPos-x; j--){
+                    block[j] = 'W';
+                }
+                curPos = curPos -x-1;
+            }
+        }
+
+        int w = 0;
+        int b = 0;
+
+        for(char bl : block){
+            if(bl == 'W') w++;
+            if(bl == 'B') b++;
+        }
+
+        System.out.println(w + " " + b);
+
+    }
+}
