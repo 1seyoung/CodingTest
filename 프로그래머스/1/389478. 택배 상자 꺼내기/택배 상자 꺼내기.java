8b667d710bfb478bfe35c5fa673dd3b5e15e33ea
Seyoung Han <89440450+1seyoung@users.noreply.github.com>
2025-11-30 04:42:08 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 10.01 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int w, int num) {
+        List<Integer> answers =  new ArrayList<>();
+        int answer = 0;
+        // n -> 개수, w -> 가로에 놓는 개수,num -> 꺼낼 것
+                
+        // num 은 어느 줄에 있을까?
+        int numRow = 0;
+        if(num % w == 0){
+            numRow = (num / w) ;
+        } else {
+            numRow = (num / w) +1;
+        }
+        
+        // numRow 가 짝수 열이다 ->
+        
+        while(true){
+            if(numRow % 2 == 1){
+
+                int sum = ((numRow) * w) + ((numRow) * w) +1;
+                System.out.println("!!! "+sum);
+                num = sum - num;
+                System.out.println("!!! "+num);
+                if(num <= n){
+                    answers.add(num);
+                    numRow += 1;
+                } else {
+                    break;
+                }
+
+            } else  {
+                int sum = (((numRow-1) * w) + 1) + ((numRow+1) * w) ;
+                
+                System.out.println("??? "+sum);
+
+                num = sum - num;
+                System.out.println("??? "+num);
+                if(num <= n){
+                    answers.add(num);
+                    numRow += 1;
+                }  else{
+                    break;
+                }               
+            }
+        }
+        
+        for(int ss : answers){
+            //System.out.println(ss);
+        }
+
+        return answers.size() +1;
+    }
+}
