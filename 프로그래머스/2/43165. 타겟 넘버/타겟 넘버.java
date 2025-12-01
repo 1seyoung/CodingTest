5f2b618e8a39921f6ea5c7727bc421a636d4d4d8
Seyoung Han <89440450+1seyoung@users.noreply.github.com>
2025-12-01 09:28:17 +0900

    [level 2] Title: 타겟 넘버, Time: 5.22 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,6 +1,23 @@
 class Solution {
+    int count = 0;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        return answer;
+        
+        dfs(numbers, target, 0, 0);
+            
+            return count;
+    }
+    
+    void dfs(int[] numbers, int target, int index, int sum ) {
+        if(index == numbers.length) {
+            if(sum == target) {
+                count ++;
+            }
+            return;
+        }
+        
+        dfs(numbers,target, index +1, sum + numbers[index]);
+
+        dfs(numbers,target,index +1, sum - numbers[index]);
+
     }
 }
